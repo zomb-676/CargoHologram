02753992545526ee58e13f6d78b16bd766b4e3fe
zomb_676 <zomb_676@outlook.com>
2024-03-19 22:23:38 +0800

    probably invalidate outdated Capability

--- src/main/java/com/github/zomb_676/cargo_hologram/blockEntity/CargoChargeTableBlockEntity.kt
+++ src/main/java/com/github/zomb_676/cargo_hologram/blockEntity/CargoChargeTableBlockEntity.kt
@@ -6,12 +6,13 @@ import com.github.zomb_676.cargo_hologram.util.filter.ItemTrait
 import net.minecraft.core.BlockPos
 import net.minecraft.core.Direction
 import net.minecraft.world.item.ItemStack
-import net.minecraft.world.level.Level
 import net.minecraft.world.level.block.entity.BlockEntity
 import net.minecraft.world.level.block.state.BlockState
 import net.minecraftforge.common.capabilities.Capability
 import net.minecraftforge.common.capabilities.ForgeCapabilities
 import net.minecraftforge.common.util.LazyOptional
+import net.minecraftforge.energy.EmptyEnergyStorage
+import net.minecraftforge.energy.IEnergyStorage
 import net.minecraftforge.items.ItemStackHandler
 import net.minecraftforge.items.wrapper.CombinedInvWrapper
 
@@ -21,6 +22,11 @@ class CargoChargeTableBlockEntity(pos: BlockPos, state: BlockState) :
     val chargeItemHandle = object : ItemStackHandler(1) {
         override fun isItemValid(slot: Int, stack: ItemStack): Boolean =
             stack.getCapability(CapRegisters.CARGO_ENERGY_ITEM).isPresent
+
+        override fun setStackInSlot(slot: Int, stack: ItemStack) {
+            super.setStackInSlot(slot, stack)
+            energyCap.invalidate()
+        }
     }
 
     val fuelItemHandle = object : ItemStackHandler(1) {
@@ -29,9 +35,21 @@ class CargoChargeTableBlockEntity(pos: BlockPos, state: BlockState) :
 
     val combined = CombinedInvWrapper(chargeItemHandle, fuelItemHandle)
 
+    private var energyCap: LazyOptional<out IEnergyStorage> = LazyOptional.of { EmptyEnergyStorage.INSTANCE }
+
     override fun <T : Any> getCapability(cap: Capability<T>, side: Direction?): LazyOptional<T> = when (cap) {
         ForgeCapabilities.ITEM_HANDLER -> LazyOptional.of { combined }
-        ForgeCapabilities.ENERGY -> chargeItemHandle.getStackInSlot(0).getCapability(CapRegisters.CARGO_ENERGY_ITEM)
+        ForgeCapabilities.ENERGY -> {
+            if (!energyCap.isPresent) {
+                val currentItem = chargeItemHandle.getStackInSlot(0)
+                energyCap = currentItem.getCapability(CapRegisters.CARGO_ENERGY_ITEM)
+                if (!energyCap.isPresent) {
+                    energyCap = LazyOptional.of { EmptyEnergyStorage.INSTANCE }
+                }
+            }
+            energyCap
+        }
+
         else -> LazyOptional.empty()
     }.cast()
 }
